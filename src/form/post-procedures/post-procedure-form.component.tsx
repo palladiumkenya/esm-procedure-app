bec333cbb75555c1787541d44e200b1479d81297
Donald Kibet <chelashawdonald@yahoo.com>
2024-03-28 08:38:33 +0300

    (feat) KHP3-5307 Add post procedure form

---
+++ src/form/post-procedures/post-procedure-form.component.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type PostProcedureFormProps = {};
+
+const PostProcedureForm: React.FC<PostProcedureFormProps> = () => {
+  return <div>PostProcedureForm</div>;
+};
+
+export default PostProcedureForm;
