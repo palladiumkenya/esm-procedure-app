5cb1d17759397a5415eadfb4ca6cbc86a57a1e52
jecihjoy <jecihjoy@gmail.com>
2024-04-06 17:52:02 +0300

    Code cleanup

--- src/form/procedures-orders/add-procedures-order/procedures-order.ts
+++ src/form/procedures-orders/add-procedures-order/procedures-order.ts
@@ -1,13 +1,11 @@
 import { type ProcedureOrderBasketItem } from "../../../types";
 import { type ProceduresType } from "./useProceduresTypes";
 
-// See the Urgency enum in https://github.com/openmrs/openmrs-core/blob/492dcd35b85d48730bd19da48f6db146cc882c22/api/src/main/java/org/openmrs/Order.java
 export const priorityOptions = [
   { value: "ROUTINE", label: "Routine" },
   { value: "STAT", label: "Stat" },
   { value: "ON_SCHEDULED_DATE", label: "Scheduled" },
 ];
-// TODO add priority option `{ value: "ON_SCHEDULED_DATE", label: "On scheduled date" }` once the form supports a date.
 
 export function createEmptyLabOrder(
   testType: ProceduresType,
