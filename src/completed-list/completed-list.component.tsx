f2ef796ae08f08c0dd772d5c6c74a5dee131711b
Jacinta Gichuhi <jecihjoy@gmail.com>
2024-04-10 23:35:53 +0300

    Revert "(fix) Revert code to working state after backend fix"

--- src/completed-list/completed-list.component.tsx
+++ src/completed-list/completed-list.component.tsx
@@ -42,9 +42,10 @@ export const CompletedList: React.FC<CompletedListProps> = ({
 
   // eslint-disable-next-line prefer-const
   let { workListEntries, isLoading } = useOrdersWorklist("", fulfillerStatus);
-  workListEntries = workListEntries.filter(
-    (order) => order?.procedures[0]?.status == "COMPLETED"
-  );
+  // temporarily remove procedures references
+  // workListEntries = workListEntries.filter(
+  //   (order) => order?.procedures[0]?.status == "COMPLETED"
+  // );
   const [activatedOnOrAfterDate, setActivatedOnOrAfterDate] = useState("");
   const pageSizes = [10, 20, 30, 40, 50];
   const [currentPageSize, setPageSize] = useState(10);
