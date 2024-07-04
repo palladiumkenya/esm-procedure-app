cf682e9ac87139c26584101f7c7cc056b87cc9d4
Charity Mulei <kokicharity3@gmail.com>
2024-07-04 15:14:27 +0300

    (Enhancer)ProcedureApp:Remove metric tiles, Make Metrics part of tab headers, Fixing the procedure report form dates

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,6 @@ import {
 } from "@openmrs/esm-framework";
 import { configSchema } from "./config-schema";
 import { createLeftPanelLink } from "./left-panel-link";
-import orderedTile from "./procedure-tiles/procedures-ordered-tile.component";
-import worklistTile from "./procedure-tiles/worklist-tile.component";
-import referredOutTile from "./procedure-tiles/referred-tile.component";
-import completedTile from "./procedure-tiles/completed-tile.component";
-import notDoneTile from "./procedure-tiles/not-done-tile.component";
 import workListProcedures from "./procedure-tabs/work-list-tab.component";
 import referredProcedures from "./procedure-tabs/referred-tab.component";
 import completedProcedures from "./procedure-tabs/completed-tab.component";
@@ -58,14 +53,6 @@ export const rejectProcedureOrderDialogComponent = getSyncLifecycle(
   options
 );
 
-export const worklistTileComponent = getSyncLifecycle(worklistTile, options);
-export const referredOutTileComponent = getSyncLifecycle(
-  referredOutTile,
-  options
-);
-export const completedTileComponent = getSyncLifecycle(completedTile, options);
-export const OrderedTileComponent = getSyncLifecycle(orderedTile, options);
-export const notDoneTileComponent = getSyncLifecycle(notDoneTile, options);
 export const worklistProceduresTabComponent = getSyncLifecycle(
   workListProcedures,
   options
